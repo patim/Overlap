8244087423f4c9c10b9f594c142cff06e9ef056a
patim <zalump8@wfu.edu>
2015-08-13 16:35:42 -0400

    addit rho2max func

--- Overlap.m
+++ Overlap.m
@@ -6,4 +6,31 @@ Maxim Zalutskiy
 2015
 *)
 BeginPackage["Overlap`"]
+ReadKerrQNM::usage="ReadKerrQNM[l, m, n] loads quasi-normal modes "<>
+"for given l, m and n.";
+
+Begin["Private`"]
+
+
+(* Quasi-normal modes loader*)
+ReadKerrQNM[l_,m_,n_]:=Module[{nname,mnamea,mname,lname,rawdat,Nelems,NL,
+	Lmin,Lmax,i,a,\[Omega],Alm,Allmn},
+	nname=If[n<10,"0"<>ToString[n],ToString[n]];
+	mnamea=If[Abs[m]<10,"0"<>ToString[Abs[m]],ToString[Abs[m]]];
+	mname=If[m<0,"-"<>mnamea,"+"<>mnamea];
+	lname = If[l<10,"0"<>ToString[l],ToString[l]];
+	Import[path<>"KerrQNM_"<>nname<>".h5",{"HDF5","Datasets",{"/n"<>
+		nname<>"/m"<>mname<>"/L"<>lname<>".dat"}}]
+]
+
+
+DefineInterpolations[lm_,]=
+
+
+\[Rho]2max[data_,lm_,modes_,pmodes_,\[Delta]_,a_,\[Theta]_,t0_]:=
+Conjugate[A[data,lm,modes,pmodes,\[Delta],a,\[Theta],t0]]*
+Inverse[B[modes,pmodes,\[Delta],a,\[Theta]]]*A[data,lm,modes,pmodes,\[Delta],a,\[Theta],t0]
+
+
+End[] (*End Private*)
 EndPackage[]
